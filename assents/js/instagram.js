9a238577e5d9813b15a926024c9f4ea2fd3ae60b
devang0609 <157216436+devang0609@users.noreply.github.com>
2024-02-20 22:21:03 +0530

    upload file

---
+++ assents/js/instagram.js
@@ -0,0 +1,114 @@
+
+document.addEventListener("keydown", (e) =>{
+    if(e.keyCode === 27){
+        closeback();
+    }
+});
+
+
+
+function closeback(){
+    document.getElementById('back').style.display="none";
+    document.getElementById('card1').style.display="none";
+    document.getElementById('card2').style.display="none";
+    document.getElementById('card3').style.display="none";
+    document.getElementById('card4').style.display="none";
+    document.getElementById('card5').style.display="none";
+    document.getElementById('card6').style.display="none";
+    document.getElementById('card7').style.display="none";
+    document.getElementById('card8').style.display="none";
+    document.getElementById('card9').style.display="none";
+    document.getElementById('card10').style.display="none";
+    document.getElementById('card11').style.display="none";
+    document.getElementById('card12').style.display="none";
+    document.getElementById('card13').style.display="none";
+    document.getElementById('card14').style.display="none";
+    document.getElementById('card15').style.display="none";
+    document.getElementById('card16').style.display="none";
+    document.getElementById('card17').style.display="none";
+    document.getElementById('card18').style.display="none";
+    document.getElementById('card19').style.display="none";
+    document.getElementById('card20').style.display="none";
+    
+}
+function girl1(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card1').style.display="flex";
+}
+function girl2(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card2').style.display="flex";
+}
+function girl3(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card3').style.display="flex";
+}
+function girl4(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card4').style.display="flex";
+}
+function girl5(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card5').style.display="flex";
+}
+function girl6(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card6').style.display="flex";
+}
+function girl7(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card7').style.display="flex";
+}
+function girl8(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card8').style.display="flex";
+}
+function girl9(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card9').style.display="flex";
+}
+function girl10(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card10').style.display="flex";
+}
+function girl11(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card11').style.display="flex";
+}
+function girl12(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card12').style.display="flex";
+}
+function girl13(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card13').style.display="flex";
+}
+function girl14(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card14').style.display="flex";
+}
+function girl15(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card15').style.display="flex";
+}
+function girl16(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card16').style.display="flex";
+}
+function girl17(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card17').style.display="flex";
+}
+function girl18(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card18').style.display="flex";
+}
+function girl19(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card19').style.display="flex";
+}
+function girl20(){
+    document.getElementById('back').style.display="flex";
+    document.getElementById('card20').style.display="flex";
+}
+
